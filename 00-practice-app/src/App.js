e00853dce15e0b50bf6309da19d0c9150b7d1829
isaac methner <ikemetyt2@gmail>
2022-10-27 20:06:18 -0400

    Pushing new material after the crap show

--- 00-practice-app/src/App.js
+++ 00-practice-app/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchCloths from './pages/SearchCloths';
+import SavedCloths from './pages/SavedCloths';
+import Checkout from './pages/Checkout';
+import Navbar from './navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route 
+            path='./search' 
+            element={<SearchCloths />} 
+          />
+          <Route 
+            path='/saved' 
+            element={<SavedCloths />} 
+          />
+          <Route 
+          path='/Checkout'
+          element ={<Checkout />}
+          />
+          <Route 
+         path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
---
+++ 00-practice-app/src/navbar.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const navbar = () => {
+    return (
+        <div>
+            navbar
+        </div>
+    )
+}
+
+export default navbar 

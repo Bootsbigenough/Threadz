1b1c7fdcec36281d34b2b7bb8a7ba9a83c725181
isaac methner <ikemetyt2@gmail>
2022-10-29 12:01:37 -0400

    Restoring broken history

---
+++ 00-practice-app/src/pages/Checkout.js
@@ -0,0 +1,19 @@
+//step 1
+
+
+
+
+
+//step 2 
+//first create button with "on click" to call function 
+
+
+// <button 
+// floated='left'
+// size='small'
+// color='Blue'
+// onClick={goToCheckout}
+// >
+//     Checkout
+// </button>
+
